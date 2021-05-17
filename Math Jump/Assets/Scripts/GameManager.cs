98526fb6e8bd8ddacbfa9bde4ea3cd657012ab32
Asamat Tschimaev <43432146+tschasam@users.noreply.github.com>
2021-05-17 02:20:29 +0200

    1

---
+++ Math Jump/Assets/Scripts/GameManager.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Problem[] problems;      // list of all problems
+    public int curProblem;          // current problem the player needs to solve
+    public float timePerProblem;    // time allowed to answer each problem
+
+    public float remainingTime;     // time remaining for the current problem
+
+
+    // instance
+    public static GameManager instance;
+
+    void Awake ()
+    {
+        // set instance to this script.
+        instance = this;
+    }
+
+    void Start ()
+    {
+        // set the initial problem
+        SetProblem(0);
+    }
+
+    void Update ()
+    {
+        remainingTime -= Time.deltaTime;
+
+        // has the remaining time ran out?
+        if(remainingTime <= 0.0f)
+        {
+            
+        }
+    }
+
+    // called when the player enters a tube
+    public void OnPlayerEnterTube (int tube)
+    {
+        // did they enter the correct tube?
+        if (tube == problems[curProblem].correctTube)
+            CorrectAnswer();
+        else
+            IncorrectAnswer();
+    }
+
+    // called when the player enters the correct tube
+    void CorrectAnswer()
+    {
+        // is this the last problem?
+        if (problems.Length - 1 == curProblem)
+            return;
+        else
+            SetProblem(curProblem + 1);
+    }
+
+    // called when the player enters the incorrect tube
+    void IncorrectAnswer ()
+    {
+        ProblemTube.Destroy(this);
+    }
+
+    // sets the current problem
+    void SetProblem (int problem)
+    {
+        curProblem = problem;
+        UI.instance.SetProblemText(problems[curProblem]);
+        remainingTime = timePerProblem;
+    }
+
+}
---
+++ Math Jump/Assets/Scripts/UI.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Text problemText;                // text that displays the maths problem
+    public Text[] answersTexts;             // array of the 4 answers texts
+
+
+
+    // instance
+    public static UI instance;
+
+    void Awake ()
+    {
+        // set instance to be this script
+        instance = this;
+    }
+    
+
+
+    // sets the ship UI to display the new problem
+    public void SetProblemText (Problem problem)
+    {
+        string operatorText = "";
+
+        // convert the problem operator from an enum to an actual text symbol
+        switch(problem.operation)
+        {
+            case MathsOperation.Addition: operatorText = " + "; break;
+            case MathsOperation.Subtraction: operatorText = " - "; break;
+            case MathsOperation.Multiplication: operatorText = " x "; break;
+            case MathsOperation.Division: operatorText = " ÷ "; break;
+        }
+
+        // set the problem text to display the problem
+        problemText.text = problem.firstNumber + operatorText + problem.secondNumber;
+
+        // set the answers texts to display the correct and incorrect answers
+        for(int index = 0; index < answersTexts.Length; ++index)
+        {
+            answersTexts[index].text = problem.answers[index].ToString();
+        }
+    }
+
+
+}
